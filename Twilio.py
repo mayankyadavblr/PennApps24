0e1382d50ece91abbfdf14c3419cf840f5f800df
mayankyadavblr <prehistoric0001@gmail.com>
2023-09-09 18:16:28 -0400

    twilio working

--- Twilio.py
+++ Twilio.py
@@ -1,7 +1,7 @@
 # Importing required libraries
 from datetime import datetime
 import matplotlib.pyplot as plt
-from meteostat import Point, Monthly
+from meteostat import Point, Daily, Monthly
 from statistics import mean
 import os
 from twilio.rest import Client
@@ -15,7 +15,7 @@ def collect_weather(lat, long):
     location = Point(lat, long)
 
     # Get daily data for 2022
-    data = Monthly(location, start, end)
+    data = Daily(location, start, end)
     data = data.fetch()
     try:
         temp_avg = mean(data["tavg"])
@@ -26,16 +26,16 @@ def collect_weather(lat, long):
         return None, None
 
 def send_sms(message_body):
-    account_sid = os.environ['ACa1724f4ca98de87b1ed387eec79e157d']
-    auth_token = os.environ['[AuthToken]']
+    account_sid = 'ACa1724f4ca98de87b1ed387eec79e157d'
+    auth_token = 'd3c67ebb320d8d253661546144e0fe0c'
     
     client = Client(account_sid, auth_token)
 
     message = client.messages \
                 .create(
                      body=message_body,
-                     from_='YOUR-TWILI0-NUMBER',
-                     to='+18149549696'
+                     from_='+18335095612',
+                     to='+16504715598'
                  )
     print(message.sid)
 
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     temperature, precipitation = collect_weather(19.113798345335507, 72.87895812770725)
     
     if temperature and precipitation:
-        message_body = f'Average Temperature for 2022: {temperature:.2f}°C\nTotal Precipitation for 2022: {precipitation:.2f}mm'
+        message_body = f'Average Temperature for today: {temperature:.2f}°C\nTotal Precipitation for today: {precipitation:.2f}mm'
         send_sms(message_body)
     else:
         print("Failed to fetch weather data.")
