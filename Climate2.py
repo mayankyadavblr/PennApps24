3d3509c4033a6dae1cc0c62255789b43a35d968b
mayankyadavblr <prehistoric0001@gmail.com>
2023-09-09 03:07:12 -0400

    working climate api

--- Climate2.py
+++ Climate2.py
@@ -8,10 +8,10 @@ start = datetime(2018, 1, 1)
 end = datetime(2018, 12, 31)
 
 # Create Point for Vancouver, BC
-location = Point(49.2497, -123.1193, 70)
+location = Point(39.995837624559755, -75.1358451695366, 70)
 
 # Get daily data for 2018
 data = Daily(location, start, end)
 data = data.fetch()
 
-print(data)
+print(data[["tavg", "prcp"]])
