68e5cdd56742c4bc4b56581abdaa7149066411b2
vishalbidari2002 <91538501+vishalbidari2002@users.noreply.github.com>
2023-09-09 09:09:23 -0400

    Add files via upload

---
+++ import arrow.py
@@ -0,0 +1,23 @@
+import arrow
+import requests
+import json
+
+start = arrow.get('2021-05-15')
+end = arrow.get('2023-05-15')
+
+response = requests.get(
+  'https://api.stormglass.io/v2/bio/point',
+  params={
+    'lat': 58.7984,
+    'lng': 17.8081,
+    'params': ','.join(['iron', 'nitrate','soilMoisture','soilMoisture10cm','soilMoisture40cm','soilMoisture100cm','soilTemperature','soilTemperature10cm','soilTemperature40cm',
+    'soilTemperature100cm']),
+    'start': start.to('UTC').timestamp(),  # Convert to UTC timestamp
+    'end': end.to('UTC').timestamp(),      # Convert to UTC timestamp
+  },
+  headers={
+    'Authorization': '2d246e16-4ebe-11ee-92e6-0242ac130002-2d246e84-4ebe-11ee-92e6-0242ac130002'
+  }
+)
+
+print(json.dumps(response.json(), indent=4))
