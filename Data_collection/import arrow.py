57c8e649f9ca99f06b37b0ae5805ff229d26322e
mayankyadavblr <prehistoric0001@gmail.com>
2023-09-09 16:33:34 -0400

    updated data

--- Data_collection/import arrow.py
+++ Data_collection/import arrow.py
@@ -3,19 +3,19 @@ import requests
 import json
 
 start = arrow.get('2021-05-15')
-end = arrow.get('2023-05-15')
+end = arrow.get('2021-05-17')
 
 response = requests.get(
   'https://api.stormglass.io/v2/bio/point',
   params={
-    'lat': 58.7984,
-    'lng': 17.8081,
-    'params': ','.join(['iron', 'nitrate','soilMoisture', 'soilTemperature']),
+    'lat': 39.99123863014912,   
+    'lng': -75.20719492025617 ,
+    'params': ','.join(['ph']),
     'start': start.to('UTC').timestamp(),  # Convert to UTC timestamp
     'end': end.to('UTC').timestamp(),      # Convert to UTC timestamp
   },
   headers={
-    'Authorization': '2d246e16-4ebe-11ee-92e6-0242ac130002-2d246e84-4ebe-11ee-92e6-0242ac130002'
+    'Authorization': 'df8ed950-4f3c-11ee-a26f-0242ac130002-df8ed9e6-4f3c-11ee-a26f-0242ac130002'
   }
 )
 
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from Climate2 import *
+import random
+import numpy as np
+
+
+def Ph_of_soil():
+    return random.choice(np.arange(4.5, 8.5, 0.007)) 
+
 def main(lat, lang):
-    pass
+    temperature, precipitation = collect_weather(lat, lang)
+    Ph =  Ph_of_soil()
+    
 
 if __name__ == '__main__':
     main()
