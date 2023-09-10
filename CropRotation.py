b6a829061f80668ef16ff28c5ae5afbf9dfc1530
vishalbidari2002 <91538501+vishalbidari2002@users.noreply.github.com>
2023-09-10 00:42:18 -0400

    Add files via upload

--- CropRotation.py
+++ CropRotation.py
@@ -1,32 +1,28 @@
 import openai
-import os
-import pandas as pd
-import time
 
-openai.api_key = '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'
+# Your OpenAI API Key
+API_KEY = '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'
+openai.api_key = API_KEY
 
-def main_rotation(crops):
+# Mock crop database for demonstration
+CROP_ROTATION_LIST = ['wheat', 'mangoes', 'apples', 'grapes', 'orange']
 
-    a="what is the best crop rotation method that should be used with {}, {}, {}, {} and {} in each year? make it into a table format with one of the columns as the seasons. Just give the table. Prioritize first crop in the list since it will earn more money".format(crops[4], crops[3], crops[2], crops[1], crops[0])
+# Query the LLM via OpenAI API for crop rotation table
+def get_crop_rotation(crops):
+    prompt = "what is the best crop rotation method that should be used with {}, {}, {}, {} and {} in each year? " \
+             "make it into a table format with one of the columns as the seasons. Just give the table. " \
+             "Prioritize first crop in the list since it will earn more money".format(crops[4], crops[3], crops[2], crops[1], crops[0])
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=300)
+    return response.choices[0].text.strip()
 
-    prompt = a
+def main():
+    print("Crops for rotation are:")
+    for crop in CROP_ROTATION_LIST:
+        print("- {}".format(crop))
 
-    response = get_completion(prompt)
-    return response
+    rotation_table = get_crop_rotation(CROP_ROTATION_LIST)
+    print("\nCrop Rotation Table:")
+    print(rotation_table)
 
-def get_completion(prompt, model="ada"):
-
-    messages = [{"role": "user", "content": prompt}]
-
-    response = openai.ChatCompletion.create(
-
-    model=model,
-
-    messages=messages,
-
-    temperature=0,
-
-    )
-
-    return response.choices[0].message["content"]
-
+if __name__ == "__main__":
+    main()
